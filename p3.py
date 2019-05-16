75eb2112b58318096c7f0e4bd192ce40fa419f78
Hawkinsonb <hawkinson.w.b@gmail.com>
2019-05-15 21:39:56 -0700

    Mike and I finished experiment 8

--- p3.py
+++ p3.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+import random
 from matplotlib import pyplot as plt
 
 import sklearn.preprocessing
@@ -133,3 +134,111 @@ for val in y_train:
 print(f'y_train contains {zeros} zeros and {ones} ones')
 guess0Predict = zeros/(ones+zeros)
 print(guess0Predict)
+
+y_probs = np.full((len(y_train), 1), 0)
+
+fpr, tpr, thresholds = roc_curve(y_train, y_probs)
+auc = roc_auc_score(y_train, y_probs)
+print(auc)
+
+
+# Experiment 8
+trueVals = []
+falseVals = []
+newVals = []
+
+# Get values by their class and save
+for i in range(len(y_train)):
+    if y_train[i] == 1:
+        trueVals.append(X_train[i])
+    else:
+        falseVals.append(X_train[i])
+
+# Randomly pick data from old class 1 to make the size of class 0 and class 1 equal.      
+newTrueVals = trueVals
+while len(newTrueVals) < len(falseVals):
+    newTrueVals.append(trueVals[(random.randint(0,len(trueVals)-1))])
+
+# Reconstruct the data set
+newTrueY = [1] * len(newTrueVals)
+newFalseY = [0] * len(falseVals)
+
+newX_train = newTrueVals + falseVals
+newY_train = newTrueY + newFalseY
+
+x_train_unshuffled = np.array(newX_train)
+y_train_unshuffled = np.array(newY_train)
+
+# still need to shuffle the data
+indexList = np.array(range(0, len(x_train_unshuffled)))
+np.random.shuffle(indexList)
+
+x_train = []
+y_train = []
+for i in indexList:
+    x_train.append(x_train_unshuffled[i])
+    y_train.append(y_train_unshuffled[i])
+
+#for formality lets keep the 
+train_size = int(0.8 * len(x_train))
+test_size = len(x_train) - train_size
+
+# Note the variable change to fit copy pasted experiments 2-5
+X_train, X_test = x_train[:train_size], x_train[train_size:]
+y_train, y_test = y_train[:train_size], y_train[train_size:]
+
+# Experiment 2
+nb_clf = GaussianNB()
+nb_clf.fit(X_train, y_train)
+
+# Experiment 3
+#
+# a
+print('NB score: ', nb_clf.score(X_test, y_test))
+
+# b
+y_pred = nb_clf.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+
+# c, d
+#
+
+# needs double checking
+
+y_probs = nb_clf.predict_proba(X_test)
+fpr, tpr, thresholds = roc_curve(y_test, y_probs[:, 1])
+auc = roc_auc_score(y_test, y_probs[:, 1])
+
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label=f'ROC curve, AUC = {auc:.3f}')
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic for GaussianNB')
+plt.legend(loc="lower right")
+plt.show()
+
+
+# Experiment 4
+linreg_clf = LogisticRegression(
+    fit_intercept=False, solver='lbfgs', random_state=1)
+linreg_clf.fit(X_train, y_train)
+print('linreg score: ', linreg_clf.score(X_test, y_test))
+
+# Experiment 5
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+
+svc_results = dict()
+for kernel in kernels:
+    svc = SVC(kernel=kernel, gamma='scale',
+              random_state=1).fit(X_train, y_train)
+    svc_results[kernel] = svc.score(X_test, y_test)
+
+print('Best performing kernel: ', max(
+    zip(svc_results.values(), svc_results.keys())))
+
+print(svc_results.values())
